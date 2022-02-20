a44420b1486adeb374f2f2cef90e139be060588a
chu-cha-chu <mauryasatish855@gmail.com>
2022-02-20 11:19:57 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ExampleConstructor
+{
+    class Car
+    {
+        string carModel;
+        int carYear;
+
+        public Car(string model,int Year)
+        {
+            carModel = model;
+            carYear = Year;
+        }
+        public Car(int Year, string model)
+        {
+            carModel=model;
+            carYear=Year;
+        }
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("mustang", 1996);
+            Console.WriteLine(myCar.carYear);
+            Console.ReadLine(); 
+        }
+
+    }
+    
+
+}
